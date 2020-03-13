3e376454bdca21bc7be1f1a8250bc4689c194943
daughtryz <dimitar.iliev4321@abv.bg>
2020-03-13 18:06:03 +0200

    AnotherTest

---
+++ Data/TMXN.Data.Models/MigTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMXN.Data.Models
+{
+   public class MigTest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
