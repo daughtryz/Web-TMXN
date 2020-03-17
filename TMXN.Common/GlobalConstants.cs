dadb9e5f496f73f4e2ebf5d340c0c0793b3c85f3
daughtryz <dimitar.iliev4321@abv.bg>
2020-03-17 19:56:22 +0200

    Updated News

--- TMXN.Common/GlobalConstants.cs
+++ TMXN.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "TMXN";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string NewsFeeds = "NewsFeed";
     }
 }
---
+++ Web/TMXN.Web/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMXN.Web.Controllers
+{
+    public class NewsController : BaseController
+    {
+
+        public IActionResult Info()
+        {
+            return this.View();
+        }
+
+    }
+}
