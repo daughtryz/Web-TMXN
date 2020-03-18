d970e143dadc5dd0666517938e0ac5174b7ef1b0
daughtryz <dimitar.iliev4321@abv.bg>
2020-03-18 16:50:55 +0200

    MigAdded

---
+++ Data/TMXN.Data/Migrations/20200318105256_ChangedAwardRelation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TMXN.Data.Migrations
+{
+    public partial class ChangedAwardRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TeamId",
+                table: "Awards",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Awards_TeamId",
+                table: "Awards",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Awards_Teams_TeamId",
+                table: "Awards",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Awards_Teams_TeamId",
+                table: "Awards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Awards_TeamId",
+                table: "Awards");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Awards");
+        }
+    }
+}
